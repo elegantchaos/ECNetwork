2cdd84cd98c4e8b278eb3c78443bc573f5b0ea60
Sam Deane <sam@elegantchaos.com>
2013-02-19 23:17:15 +0000

    Added framework header

---
+++ Source/Mac/ECNetwork.h
@@ -0,0 +1,11 @@
+// --------------------------------------------------------------------------
+//  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECLaunchD.h"
+#import "ECMachPorts.h"
+#import "ECUnixPorts.h"
+#import "ECNetworkError.h"
+#import "ECListener.h"
